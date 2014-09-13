3bce6ed10fa1134c1c7eb39d741695ec39819414
Gwendolyn Quint <DrKwint@gmail.com>
2014-09-12 19:09:52 -0500

    Implemented CSV decoding

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate csv;
+extern crate serialize;
+
+use std::path::Path;
+
+#[deriving(Decodable)]
+struct Record {
+    s1: String,
+    s2: String,
+    s3: String,
+    s4: String,
+    s5: String,
+    s6: String,
+    s7: String,
+    s8: String,
+    s9: String,
+    s10: String,
+    s11: String,
+    s12: String,
+    s13: String,
+    s14: String,
+    s15: String,
+    s16: String,
+    s17: String,
+}
+
+fn main() {
+   let fp = &Path::new("../data/house-votes-84.data");
+   let mut rdr = csv::Decoder::from_file(fp);
+
+   for record in rdr.iter_decode::<Record>() {
+       let record = record.unwrap();
+       println!("{}", record.s1);
+   }
+}
