dec28327fe17584078f74ebdc5518b9bb6ff88b9
Gwendolyn Quint <DrKwint@gmail.com>
2014-09-25 13:33:15 -0500

    Fixed id3 implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #[experimental]
 #[doc = "ID3 decision tree implementation"]
 pub mod id3 {
+    use std::collections::HashSet;
     use std::collections::TreeMap;
+    use std::str::eq_slice;
     use std::f64;
 
     #[doc = "A vertex in a decision tree."]
+    #[deriving(Show)]
     pub enum DecisionVertex {
         /// Vertex at which the example set is split along an attribute.
         Branch(String, TreeMap<String,DecisionVertex>),
@@ -33,44 +36,52 @@ pub mod id3 {
 
     This is, in its present form, an incomplete and relatively untested implementation. It may fail unexpectedly. Do not use in a production setting.
     "]
-    pub fn id3(dataset: Vec<&Record>, label_attribute_name: &str) -> DecisionVertex {
+    pub fn id3<T: Record>(dataset: Vec<&T>, label_attribute_name: &str, attributes: Vec<&str>, entropy_threshold: f64) -> DecisionVertex {
+
         // Return labeled leaf if all labels in dataset are equal
         if attr_all_eq(&dataset, label_attribute_name) {
-            return Leaf(label_attribute_name.into_string());
+            return Leaf(dataset.iter().next().unwrap().get_attribute(label_attribute_name).to_string());
         }
+
+        // IN PROGRESS
+        // Return labeled leaf if entropy of dataset is below threshold
+        //if entropy(&dataset, label_attribute_name) < entropy_theshold {
+            
+        //}
 
         // Choose attribute to split on
         // Assumes split_attribute() is Some
-        let split_attr_name = split_attribute(&dataset, label_attribute_name).unwrap();
-        let mut branch_map = TreeMap::new();
+        let split_attr_name = split_attribute(&dataset, label_attribute_name, &attributes).unwrap();
 
-        let attr_domain: Vec<&str> = dataset.iter().map(|x| (x.get_attribute(split_attr_name.as_slice()))).collect();
+        let mut branch_map = TreeMap::new();
+        // Add a new branch for each possible attribute value
+        let attr_domain: HashSet<&str> = dataset.iter().map(|x| x.get_attribute(split_attr_name.as_slice())).collect();
         for attr_value in attr_domain.iter() {
-            let example_subset: Vec<&Record> = dataset.iter().filter(|x| x.get_attribute(split_attr_name.as_slice()) == *attr_value).map(|x| *x).collect();
-            let child_vertex = id3(example_subset, label_attribute_name);
+            let example_subset: Vec<&T> = dataset.iter().filter(|x| eq_slice(x.get_attribute(split_attr_name.as_slice()), *attr_value)).map(|x| *x).collect();
+            let attrs_left = attributes.iter().filter(|x| !eq_slice(**x, split_attr_name.as_slice())).map(|x| *x).collect();
+            let child_vertex = id3(example_subset, label_attribute_name, attrs_left, entropy_threshold);
             branch_map.insert(attr_value.to_string(), child_vertex);
         }
-        
+
         Branch(split_attr_name.to_string(), branch_map)
     }
 
     // Checks the given attribute for equality among all records
-    fn attr_all_eq(records: &Vec<&Record>, attr_name: &str) -> bool {
+    fn attr_all_eq<T: Record>(records: &Vec<&T>, attr_name: &str) -> bool {
         let first_attr = records[0].get_attribute(attr_name);
-        let all_eq = records.iter().map(|x| x.get_attribute(attr_name)).all(|x| first_attr.cmp(&x) == Equal);
+        let all_eq = records.iter().map(|x| x.get_attribute(attr_name)).all(|x| eq_slice(first_attr,x));
 
         all_eq
     }
 
     // assumes records.iter().next() is Some
-    fn split_attribute(records: &Vec<&Record>, label_attr_name: &str) -> Option<String> {
+    fn split_attribute<T: Record>(records: &Vec<&T>, label_attr_name: &str, attributes: &Vec<&str>) -> Option<String> {
         let mut min_entropy = f64::MAX_VALUE;
         let mut best_attr = None;
 
-        let attr_names = records.iter().next().unwrap().get_attribute_names();
-        for attr_name in attr_names.iter() {
+        for attr_name in attributes.iter() {
             // Assuming discrete attributes
-            if attr_name == &label_attr_name { continue; }
+            if eq_slice(*attr_name, label_attr_name) { continue; }
             let attr_label_pairs: Vec<(&str, &str)> = records.iter().map(|x| (x.get_attribute(*attr_name),x.get_attribute(label_attr_name))).collect();
             let entropy = split_entropy(attr_label_pairs);
             if entropy < min_entropy {
@@ -78,7 +89,7 @@ pub mod id3 {
                 min_entropy = entropy;
             }
         }
-        
+
         best_attr
     }
 
@@ -88,24 +99,80 @@ pub mod id3 {
         let total_attr_count = attributes.iter().count() as f64;
 
         // Loop through attribute values
-        let mut attr_values = attributes.iter().map(|&x| x.val0());
-        for attr_value in attr_values {
+        let attr_values: HashSet<&str> = attributes.iter().map(|&x| x.val0()).collect();
+        for attr_value in attr_values.iter() {
             let mut attr_entropy = 0f64;
             // N_mj
-            let attr_count = attributes.iter().filter(|x| x.val0() == attr_value).count() as f64;
+            let attr_count = attributes.iter().filter(|x| eq_slice(x.val0(), *attr_value)).count() as f64;
 
             // Loop through label values
-            let mut label_values = attributes.iter().filter(|x| x.val0() == attr_value).map(|x| x.val1());
-            for label_value in label_values {
+            let label_values: HashSet<&str> = attributes.iter().filter(|x| x.val0() == *attr_value).map(|x| x.val1()).collect();
+            //DEBUG PRINT
+            //println!("{}", label_values)
+            for label_value in label_values.iter() {
                 // N^i_mj
-                let label_count = attributes.iter().filter(|x| x.val0() == attr_value).filter(|x| x.val1() == label_value).count() as f64;
+                let label_count = attributes.iter().filter(|x| eq_slice(x.val0(), *attr_value)).filter(|x| x.val1() == *label_value).count() as f64;
                 // p^i_mj
                 let label_prop = label_count / attr_count;
                 attr_entropy = attr_entropy + (label_prop * label_prop.log2())
             }
+
             total_entropy = total_entropy + ((attr_count / total_attr_count) * attr_entropy);
         }
 
         (-1f64) * total_entropy
+    }
+
+    fn entropy<T: Record>(dataset: &Vec<T>, label_attribute_name: &str) -> f64{
+        let mut total_entropy = 0f64;
+        let total_count = dataset.len() as f64;
+        let labels: Vec<&str> = dataset.iter().map(|x| x.get_attribute(label_attribute_name)).collect();
+
+        let label_values: HashSet<&&str> = labels.iter().collect();
+        for label_value in label_values.iter() {
+            let count = labels.iter().filter(|x| eq_slice(**label_value, **x)).count() as f64;
+            let proportion = count / total_count;
+            total_entropy = total_entropy + (proportion * proportion.log2());
+        }
+
+        -total_entropy
+    }
+
+    #[test]
+    fn split_entropy_empty_set() {
+        let dataset = vec![];
+        let result = split_entropy(dataset);
+
+        assert_eq!(0f64, result);
+    }
+
+    #[test]
+    fn split_entropy_set_size_one() {
+        let dataset = vec![("DrKwint", "y")];
+        let result = split_entropy(dataset);
+
+        assert_eq!(0f64, result);
+    }
+
+    #[test]
+    fn split_entropy_slides_humidity() {
+        let dataset = vec![
+            ("high", "n"),
+            ("high", "n"),
+            ("high", "y"),
+            ("high", "y"),
+            ("normal", "y"),
+            ("normal", "n"),
+            ("normal", "y"),
+            ("high", "n"),
+            ("normal", "y"),
+            ("normal", "y"),
+            ("normal", "y"),
+            ("high", "y"),
+            ("normal", "y"),
+            ("high", "n")];
+        let result = split_entropy(dataset);
+
+        assert!(0.788f64 < result && result < 0.789f64);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,99 @@
 extern crate csv;
 extern crate serialize;
+extern crate decision_tree;
 
 use std::path::Path;
+use decision_tree::id3;
 
-#[allow(dead_code)]
-#[deriving(Decodable)]
-struct Record {
-    s1: String,
-    s2: String,
-    s3: String,
-    s4: String,
-    s5: String,
-    s6: String,
-    s7: String,
-    s8: String,
-    s9: String,
-    s10: String,
-    s11: String,
-    s12: String,
-    s13: String,
-    s14: String,
-    s15: String,
-    s16: String,
-    s17: String,
+struct VotingRecord {
+    fields: Vec<String>,
 }
 
-fn main() {
-   let fp = &Path::new("../data/house-votes-84.data");
-   let mut rdr = csv::Decoder::from_file(fp);
+static class_name: &'static str = "class_name";
+static handicapped: &'static str = "handicapped";
+static water_project: &'static str = "water_project";
+static budget_resolution: &'static str = "budget_resolution";
+static physician_freeze: &'static str = "physician_freeze";
+static el_salvador_aid: &'static str = "el_salvador_aid";
+static religious_schools: &'static str = "religious_schools";
+static anti_sattelite_ban: &'static str = "anti_sattelite_ban";
+static aid_to_contras: &'static str = "aid_to_contras";
+static mx_missile: &'static str = "mx_missile";
+static immigration: &'static str = "immigration";
+static synfuels_cutback: &'static str = "synfuels_cutback";
+static education_spending: &'static str = "education_spending";
+static right_to_sue: &'static str = "right_to_sue";
+static crime: &'static str = "crime";
+static duty_free_exports: &'static str = "duty_free_exports";
+static export_south_africa: &'static str = "export_south_africa";
 
-   for record in rdr.iter_decode::<Record>() {
-       let record = record.unwrap();
-       println!("{}", record.s1);
-   }
+impl VotingRecord {
+    fn new(fields: Vec<String>) -> VotingRecord {
+        VotingRecord {
+            fields: fields,
+        }
+    }
+}
+
+impl id3::Record for VotingRecord {
+    fn get_attribute(&self, attr_name: &str) -> &str {
+        let val = match attr_name {
+            class_name => &self.fields[0],
+            handicapped => &self.fields[1],
+            water_project => &self.fields[2],
+            budget_resolution => &self.fields[3],
+            physician_freeze => &self.fields[4],
+            el_salvador_aid => &self.fields[5],
+            religious_schools => &self.fields[6],
+            anti_sattelite_ban => &self.fields[7],
+            aid_to_contras => &self.fields[8],
+            mx_missile => &self.fields[9],
+            immigration => &self.fields[10],
+            synfuels_cutback => &self.fields[11],
+            education_spending => &self.fields[12],
+            right_to_sue => &self.fields[13],
+            crime => &self.fields[14],
+            duty_free_exports => &self.fields[15],
+            export_south_africa => &self.fields[16],
+            _ => &self.fields[17], // dirty, error throwing hack
+        };
+        val.as_slice()
+    }
+
+    fn get_attribute_names(&self) -> Vec<&str> {
+        vec![handicapped,water_project,budget_resolution,physician_freeze,
+             el_salvador_aid,religious_schools,anti_sattelite_ban,
+             aid_to_contras,mx_missile,immigration,synfuels_cutback,
+             education_spending,right_to_sue,crime,duty_free_exports,
+             export_south_africa]
+    }
+}
+
+impl<'a> id3::Record for &'a VotingRecord {
+    fn get_attribute(&self, attr_name: &str) -> &str {
+        self.get_attribute(attr_name)
+    }
+
+    fn get_attribute_names(&self) -> Vec<&str> {
+        self.get_attribute_names()
+    }
+}
+
+#[cfg(not(test))]
+fn main() {
+    let fp = &Path::new("./data/voting-records/house-votes-84.data");
+    let mut rdr = csv::Reader::from_file(fp);
+    let rows = csv::collect(rdr.records()).unwrap();
+    let records = rows.map_in_place(|x| VotingRecord::new(x));
+    let attr_names = vec![handicapped,water_project,budget_resolution,
+                          physician_freeze,el_salvador_aid,religious_schools,
+                          anti_sattelite_ban,aid_to_contras,mx_missile,
+                          immigration,synfuels_cutback,education_spending,
+                          right_to_sue,crime,duty_free_exports,
+                          export_south_africa];
+
+
+    let root_vertex = id3::id3(records.iter().collect(), class_name, attr_names,0f64);
+
+    println!("{}", root_vertex);
 }
